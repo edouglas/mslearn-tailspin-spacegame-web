e1151e3d69f637674f56789a7bd38c422086627c
Emily Douglas <emily@mapletonhill.net>
2021-06-10 14:09:24 -0500

    Try managed

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -16,31 +17,46 @@ namespace TailSpin.SpaceGame.Web
         {
             CreateHostBuilder(args).Build().Run();
         }
-        
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureAppConfiguration((context, config) =>
+        //        {
+        //            config.AddEnvironmentVariables();
+
+        //            if (context.HostingEnvironment.IsProduction())
+        //            {
+        //                var builtConfig = config.Build();
+
+        //                using var store = new X509Store(StoreLocation.CurrentUser);
+        //                store.Open(OpenFlags.ReadOnly);
+        //                var certs = store.Certificates.Find(
+        //                    X509FindType.FindByThumbprint,
+        //                    builtConfig["AzureADCertThumbprint"], false);
+
+        //                if (certs.Count > 0)
+        //                {
+        //                    config.AddAzureKeyVault(new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+        //                        new ClientCertificateCredential(builtConfig["AzureADDirectoryId"], builtConfig["AzureADApplicationId"], certs.OfType<X509Certificate2>().Single()),
+        //                        new KeyVaultSecretManager());
+        //                }
+
+        //                store.Close();
+        //            }
+        //        })
+        //        .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    config.AddEnvironmentVariables();
-
                     if (context.HostingEnvironment.IsProduction())
                     {
                         var builtConfig = config.Build();
-
-                        using var store = new X509Store(StoreLocation.CurrentUser);
-                        store.Open(OpenFlags.ReadOnly);
-                        var certs = store.Certificates.Find(
-                            X509FindType.FindByThumbprint,
-                            builtConfig["AzureADCertThumbprint"], false);
-
-                        if (certs.Count > 0)
-                        {
-                            config.AddAzureKeyVault(new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
-                                new ClientCertificateCredential(builtConfig["AzureADDirectoryId"], builtConfig["AzureADApplicationId"], certs.OfType<X509Certificate2>().Single()),
-                                new KeyVaultSecretManager());
-                        }
-
-                        store.Close();
+                        var secretClient = new SecretClient(
+                            new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+                            new DefaultAzureCredential());
+                        config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
                     }
                 })
                 .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
